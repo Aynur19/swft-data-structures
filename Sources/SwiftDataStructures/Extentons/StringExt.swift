51b5a5a24c3d131b68e33e787a0aee82221a3824
Aynur Nasybullin <nasybullin.aynur@yandex.ru>
2024-03-20 11:20:56 +0300

    added: longestCommonPrefix() func;

--- Sources/SwiftDataStructures/Extentons/StringExt.swift
+++ Sources/SwiftDataStructures/Extentons/StringExt.swift
@@ -6,6 +6,7 @@
 //
 
 extension String {
+    // MARK: LeetCode Problem 1750. Minimum Length of String After Deleting Similar Ends
     // LeetCode problem: https://leetcode.com/problems/minimum-length-of-string-after-deleting-similar-ends/
     // Approach: Two Pointers
     // Time complexity: O(n)
@@ -39,5 +40,32 @@ extension String {
             
             if chars[left] != chars[right] { return right - left + 1 }
         }
+    }
+    
+    
+    // MARK: LeetCode Problem 14. Longest Common Prefix
+    // Link: https://leetcode.com/problems/longest-common-prefix/
+    // Approach: Pointer, String, Array
+    // Time complexity: O(m * n)
+    // Space complexity: O(1)
+    public static func longestCommonPrefix(_ strs: [String]) -> String {
+        guard var common = strs.min() else { return "" }
+        var containsCount = 0
+        
+        while !common.isEmpty {
+            containsCount = 0
+            
+            for i in 0..<strs.count {
+                if strs[i].hasPrefix(common) {
+                    containsCount += 1
+                } else { break }
+            }
+            
+            if containsCount == strs.count { break }
+            
+            common.removeLast()
+        }
+        
+        return containsCount == strs.count ? common : ""
     }
 }
--- Tests/SwiftDataStructuresTests/ExtentionsTests/StringExtTests.swift
+++ Tests/SwiftDataStructuresTests/ExtentionsTests/StringExtTests.swift
@@ -8,8 +8,8 @@
 import XCTest
 
 final class StringExtTests: XCTestCase {
-    // MARK: LeetCode Problem 238. Product of Array Except Self
-    
+    // MARK: LeetCode Problem 1750. Minimum Length of String After Deleting Similar Ends
+    // LeetCode problem: https://leetcode.com/problems/minimum-length-of-string-after-deleting-similar-ends/
     lazy var testsData_minLengthDiffPrefixSuffix: [(s: String, expected: Int)] = {
         var testsData = [(s: String, expected: Int)]()
         
@@ -19,18 +19,38 @@ final class StringExtTests: XCTestCase {
         testsData.append((s: "bbbbbbbbbbbbbbbbbbbbbbbbbbbabbbbbbbbbbbbbbbccbcbcbccbbabbb", expected: 1))
         testsData.append((s: "abbbbbbbbbbbbbbbbbbba", expected: 0))
         testsData.append((
-            s: "aabbbbbbbbbbbbbbaaaaaaacccccbcbcccbbbccbbbcbcbccacbccccbbabacbaccaccbbcabbbbaaaaaaaaaaaaaaaaaa", 
+            s: "aabbbbbbbbbbbbbbaaaaaaacccccbcbcccbbbccbbbcbcbccacbccccbbabacbaccaccbbcabbbbaaaaaaaaaaaaaaaaaa",
             expected: 36
         ))
         
         return testsData
     }()
     
-    // MARK: Problem 1750. Minimum Length of String After Deleting Similar Ends
     func testsProblem_minLengthDiffPrefixSuffix() throws {
         for data in testsData_minLengthDiffPrefixSuffix {
             let message = "s: '\(data.s)'"
             let actual = data.s.minLengthDiffPrefixSuffix()
+            
+            XCTAssertEqual(actual, data.expected, message)
+        }
+    }
+    
+    
+    // MARK: LeetCode Problem 14. Longest Common Prefix
+    // Link: https://leetcode.com/problems/longest-common-prefix/
+    lazy var testsData_longestCommonPrefix: [(strs: [String], expected: String)] = {
+        var testsData = [(strs: [String], expected: String)]()
+        
+        testsData.append((strs: ["flower", "flow", "flight"], expected: "fl"))
+        testsData.append((strs: ["dog", "racecar", "car"], expected: ""))
+        
+        return testsData
+    }()
+    
+    func testsProblem_longestCommonPrefix() throws {
+        for data in testsData_longestCommonPrefix {
+            let message = "strs: '\(data.strs)'"
+            let actual = String.longestCommonPrefix(data.strs)
             
             XCTAssertEqual(actual, data.expected, message)
         }
