96e83bf30f4745b655c0992518f26c6c023e8596
Aynur Nasybullin <nasybullin.aynur@yandex.ru>
2024-03-20 11:40:14 +0300

    added: getSet3SumWithResult0() func;

--- Sources/SwiftDataStructures/Extentons/ArrayExt.swift
+++ Sources/SwiftDataStructures/Extentons/ArrayExt.swift
@@ -6,28 +6,6 @@
 //
 
 extension Array<Int> {
-    // MARK: LeetCode Problem 1. Two Sum
-    // Link: https://leetcode.com/problems/two-sum/description/
-    // Approach: Hash Table
-    // Time complexity: O(n)
-    // Space complexity: O(n)
-    public func twoSum(_ target: Int) -> [Int] {
-        guard self.count > 1 else { return [] }
-        var dict = [Int:Int]()
-        
-        for idx in self.indices {
-            let num = self[idx]
-            
-            if let index = dict[target - num] {
-                return [index, idx]
-            }
-            
-            dict[num] = idx
-        }
-        
-        return []
-    }
-    
     // MARK: LeetCode Problem 238. Product of Array Except Self
     // Link: https://leetcode.com/problems/product-of-array-except-self/
     // Approach: Array, Prefix Sum
@@ -53,4 +31,77 @@ extension Array<Int> {
     }
     
     
+    // MARK: LeetCode Problem 15. 3Sum
+    // Link: https://leetcode.com/problems/3sum/
+    // Approach: Three Pointers, Sorting
+    // Time complexity: O(n log n)
+    // Space complexity: O(1)
+    public func getSet3SumWithResult0() -> [[Int]] {
+        let nums = self.sorted()
+        
+        var result = [[Int]]()
+        var idx = 0, left = 0, right = 0
+        var last = [1, 1, 1]
+        
+        while nums[idx] < 0, idx + 2 < nums.count {
+            if idx > 0, nums[idx] == nums[idx - 1] {
+                idx += 1
+                continue
+            }
+            
+            left = idx + 1
+            right = nums.count - 1
+            
+            while left < right {
+                let value = nums[idx] + nums[left] + nums[right]
+                
+                if value == 0 {
+                    let new = [nums[idx], nums[left], nums[right]]
+                    if last != new {
+                        result.append(new)
+                        last = new
+                    }
+                    right -= 1
+                    left += 1
+                    
+                    continue
+                } else if value > 0 {
+                    right -= 1
+                } else {
+                    left += 1
+                }
+            }
+            
+            idx += 1
+        }
+        
+        if idx < nums.count - 2, nums[idx + 2] == 0 {
+            result.append([0, 0, 0])
+        }
+        
+        return result
+    }
+    
+    
+    // MARK: LeetCode Problem 1. Two Sum
+    // Link: https://leetcode.com/problems/two-sum/description/
+    // Approach: Hash Table
+    // Time complexity: O(n)
+    // Space complexity: O(n)
+    public func twoSum(_ target: Int) -> [Int] {
+        guard self.count > 1 else { return [] }
+        var dict = [Int:Int]()
+        
+        for idx in self.indices {
+            let num = self[idx]
+            
+            if let index = dict[target - num] {
+                return [index, idx]
+            }
+            
+            dict[num] = idx
+        }
+        
+        return []
+    }
 }
--- Tests/SwiftDataStructuresTests/ExtentionsTests/ArrayExtTests.swift
+++ Tests/SwiftDataStructuresTests/ExtentionsTests/ArrayExtTests.swift
@@ -8,6 +8,52 @@
 import XCTest
 
 final class ArrayExtTests: XCTestCase {
+    // MARK: LeetCode Problem 238. Product of Array Except Self
+    // Link: https://leetcode.com/problems/product-of-array-except-self/
+    lazy var testsData_productExceptSelf: [(nums: [Int], expected: [Int])] = {
+        var testsData = [(nums: [Int], expected: [Int])]()
+        
+        testsData.append((nums: [1,2,3,4], expected: [24,12,8,6]))
+        testsData.append((nums: [-1,1,0,-3,3], expected: [0,0,9,0,0]))
+        
+        return testsData
+    }()
+    
+    func tests_productExceptSelf() throws {
+        for data in testsData_productExceptSelf {
+            let message = "nums: \(data.nums)"
+            let actual = data.nums.productExceptSelf(data.nums)
+            
+            XCTAssertEqual(actual, data.expected, message)
+        }
+    }
+    
+    
+    // MARK: LeetCode Problem 15. 3Sum
+    // Link: https://leetcode.com/problems/3sum/
+    lazy var testsData_getSet3SumWithResult0: [(nums: [Int], expected:  [[Int]])] = {
+        var testsData = [(nums: [Int], expected:  [[Int]])]()
+        
+        testsData.append((nums: [-1, 0, 1, 2, -1, -4], expected: [[-1, -1, 2], [-1, 0, 1]]))
+        testsData.append((nums: [0, 1, 1], expected:  []))
+        testsData.append((nums: [0, 0, 0], expected:  [[0, 0, 0]]))
+        testsData.append((nums: [-1, 0, 1, 0], expected:  [[-1, 0, 1]]))
+        testsData.append((nums: [1, -1, -1, 0], expected:  [[-1, 0, 1]]))
+        testsData.append((nums: [-2, 0, 1, 1, 2], expected:  [[-2, 0, 2], [-2, 1, 1]]))
+        
+        return testsData
+    }()
+    
+    func tests_getSet3SumWithResult0() throws {
+        for data in testsData_getSet3SumWithResult0 {
+            let message = "nums: \(data.nums)"
+            let actual = data.nums.getSet3SumWithResult0()
+            
+            XCTAssertEqual(actual, data.expected, message)
+        }
+    }
+    
+    
     // MARK: LeetCode Problem 1. Two Sum
     // Link: https://leetcode.com/problems/two-sum/description/
     lazy var testsData_twoSum: [(nums: [Int], target: Int, expected: [Int])] = {
@@ -24,27 +70,6 @@ final class ArrayExtTests: XCTestCase {
         for data in testsData_twoSum {
             let message = "nums: \(data.nums)"
             let actual = data.nums.twoSum(data.target)
-            
-            XCTAssertEqual(actual, data.expected, message)
-        }
-    }
-    
-    
-    // MARK: LeetCode Problem 238. Product of Array Except Self
-    // Link: https://leetcode.com/problems/product-of-array-except-self/
-    lazy var testsData_productExceptSelf: [(nums: [Int], expected: [Int])] = {
-        var testsData = [(nums: [Int], expected: [Int])]()
-        
-        testsData.append((nums: [1,2,3,4], expected: [24,12,8,6]))
-        testsData.append((nums: [-1,1,0,-3,3], expected: [0,0,9,0,0]))
-        
-        return testsData
-    }()
-    
-    func tests_productExceptSelf() throws {
-        for data in testsData_productExceptSelf {
-            let message = "nums: \(data.nums)"
-            let actual = data.nums.productExceptSelf(data.nums)
             
             XCTAssertEqual(actual, data.expected, message)
         }
